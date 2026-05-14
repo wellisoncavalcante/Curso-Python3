6f50a26265cb8f2b771763b8329f6558f0571f12
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-05-14 18:03:30 -0300

    add aula22.py, aula23.py, and aula24.py for logical operators demonstration

---
+++ aula24.py
@@ -0,0 +1,9 @@
+# Operadores in e not in
+# Strings são iteráveis
+#  0  1  2  3  4  5  6  7
+#  W  e  l  l  i  s  o  n
+# -8 -7 -6 -5 -4 -3 -2 -1
+
+nome = 'Wellison'
+print(nome[5])
+print(nome[-3])
