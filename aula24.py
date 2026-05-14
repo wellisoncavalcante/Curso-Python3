c8c2e2430042870fe7322d23dfdeb5828d43b567
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-05-14 18:07:01 -0300

    aula24 - operador in e not in

--- aula24.py
+++ aula24.py
@@ -5,5 +5,22 @@
 # -8 -7 -6 -5 -4 -3 -2 -1
 
 nome = 'Wellison'
-print(nome[5])
-print(nome[-3])
+print(nome[0])
+print(nome[-8])
+print('W' in nome)
+print('Welli' in nome)
+
+print('='*100)
+
+print('W' not in nome)
+print('Welli' not in nome)
+
+print('='*100)
+
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontrar: ')
+
+if encontrar in nome:
+    print(f'{encontrar} está em {nome}')
+else:
+    print(f'{encontrar} não está em {nome}')
