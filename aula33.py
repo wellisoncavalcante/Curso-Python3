6aa2db9c556871b38926529e0b9bccc81a5060fb
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-05-16 15:54:43 -0300

    documentação, tipo imutável e métodos string

---
+++ aula33.py
@@ -0,0 +1,11 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis até agora: str, int, float, bool
+"""
+
+string = 'Wellison Cavalcante'
+variavel = f'{string[:5]}FODAO{string[8:]}'
+print(variavel)
+
+numero = '1'
+print(numero.zfill(10))
