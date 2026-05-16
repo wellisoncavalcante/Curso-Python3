11a6beb75bf5d83bec80c80393ec1891cf72edc9
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-05-16 16:10:49 -0300

    while - condição em detalhes

---
+++ aula35.py
@@ -0,0 +1,15 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+
+contador = 1
+
+while contador <= 10:
+    print('OLOQUINHO MEUU')
+    
+    contador += 1
+    
+print('Acabou pro beta')
