6d98045620fff617fce91986cb7c42483aa1927a
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-05-30 18:38:54 -0300

    enumerate para enumerar valores de iteráveis

---
+++ aula53.py
@@ -0,0 +1,9 @@
+"""
+enumerate - enumera iteráveis (índices) 
+"""
+
+lista = ['Wellison', 'Cavalcante', 'Desenvolvedor']
+lista.append('Python')
+
+for indice, nome in enumerate(lista):
+    print(indice, nome)
