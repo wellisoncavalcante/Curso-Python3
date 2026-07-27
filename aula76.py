92e5f0dd27e9c4205a7eb7fe3c6187cbdf1100b1
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-07-27 16:46:53 -0300

    Introdução ao tipo set em Python

---
+++ aula76.py
@@ -0,0 +1,10 @@
+# Sets - Conjuntos em Python (tipo set)
+# Sets em Python são mutáveis, porém aceitam apenas tipos imutáveis como valor interno.
+
+# Criando um set
+# set (iterável) ou {1, 2, 3}
+
+# s1 = set('Wellison')
+s1 = set() # vazio
+s1 = {'Wellison', 1, 2, 3} # com dados
+print(s1)
