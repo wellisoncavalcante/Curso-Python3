0e22fb448d58d9a7b56234bf9d2f641191b34ce0
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-07-30 18:30:35 -0300

    isinstance() - para saber se objeto é de determinado tipo

---
+++ aula88.py
@@ -0,0 +1,22 @@
+lista = [
+    'a', 1, 1.1, True, [0, 1, 2], (1, 2),
+         {0, 1}, {'nome': 'Wellison'},
+]
+
+for item in lista:
+    if isinstance(item, set):
+        print('SET')
+        item.add(5)
+        print(item, isinstance(item, set))
+
+    elif isinstance(item, str):
+        print('STR')
+        print(item.upper())
+
+    elif isinstance(item, (int, float)):
+        print('NUMBER')
+        print(item, item * 2)
+
+    else:
+        print('Outro')
+        print(item)
