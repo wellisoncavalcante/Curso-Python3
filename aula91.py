5796e27dd6a52b09ffd07d4b3c1e365fd346dd2f
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-08-19 15:35:39 -0300

    generator and iterator examples in Python

--- aula91.py
+++ aula91.py
@@ -1,6 +1,19 @@
+import sys
+
 # Generator expression, Iterables e Iterators em Python
 iterable = ['Eu', 'Tenho', '__iter__']
 iterator = iter(iterable) # tem __iter__ e __next__
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+lista = [n for n in range(1000000)]
+generator = (n for n in range(1000000))
+
+print(sys.getsizeof(lista)) # Salva tudo na memória
+print(sys.getsizeof(generator)) # Não salva tudo diretamente na memória, vai utilizando de acordo com o que é chamado
+
+print(generator)
+
+# for n in generator:
+#     print(n)
