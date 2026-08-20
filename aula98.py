fcd8c9465d663db73aab1fafbfcd4e8f35f4faaf
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-08-20 01:17:40 -0300

    add aula98.py module to demonstrate understanding of Python modules

---
+++ aula98.py
@@ -0,0 +1,16 @@
+# Entendendo os seus próprios módulos Python
+# O primeiro módulo executado chamase __main__
+# Você pode importar outro módulo inteiro ou parte do módulo
+# O python conhece a pasta onde o __main__ está e as pastas abaixo dele
+# Ele não reconhece pastas e módulos acima do __main__ por padrão
+# O python conhece todos os módulos e pacotes presentes nos caminhos
+# de sys.path
+try:
+    import sys
+    sys.path.append('/home')
+except ModuleNotFoundError:
+    ...
+import aula98_m
+
+print('Este módulo se chama', __name__)
+print(*sys.path, sep='\n')
