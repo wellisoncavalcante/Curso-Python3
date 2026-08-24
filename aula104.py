69c5489275b00f8afad5d97ea5b4887cac74dee0
wellisoncavalcante <wellisoncavalcante46@gmail.com>
2026-08-24 19:19:12 -0300

    decoradores

--- aula104.py
+++ aula104.py
@@ -3,16 +3,22 @@
 # Funções decoradoras são funções que decoram outras funções
 # Decoradores são usados para fazer o Python
 # Usar as funções decoradoras em outras funções.
+# Decoradores são "Syntax Sugar" (Açúcar sintático)
 
 def create_function(func):
     def internal(*args, **kwargs):
+        print('Vou te decorar')
         for arg in args:
             is_string(arg)
         result = func(*args, **kwargs)
+        print(f'O seu resultado foi {result}.')
+        print('Ok, agora você foi decorada')
         return result
     return internal
 
-def invert_function(string):
+# @create_function
+def invert_string(string):
+    print(f'{invert_string.__name__}')
     return string[::-1]
 
 def is_string(param):
@@ -20,5 +26,5 @@ def is_string(param):
         raise TypeError('param deve ser uma string')
 
 
-inverted = invert_function('Wellison')
+inverted = invert_string('Wellison')
 print(inverted)
